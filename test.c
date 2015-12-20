3c0ede26ded4fe8146254511519bc85dec47eb35
naviso <aerohead72@gmail.com>
2015-12-20 23:45:21 +1100

    Added text display on console to read I/O and errors

---
+++ test.c
@@ -0,0 +1,12 @@
+#include "test.h"
+
+int PI2 = 1;
+
+void test_printing(void)
+{
+	printf("Rand Value: %f\r", rand() * 0.1);
+}
+
+
+
+
---
+++ test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdint.h>
+
+extern PI2;
+
+void test_printing(void);
+
+#endif
